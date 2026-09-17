c27fd35abb8272e46a50d7e28d90ffb3f0448afe
Uriel Juliatti Valle <ujvalle@gmail.com>
2026-09-17 09:18:37 -0300

    Add Band Admin product management

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,8 @@ class Product < ApplicationRecord
   belongs_to :band
   has_many :variants, class_name: "ProductVariant", dependent: :destroy
 
+  accepts_nested_attributes_for :variants
+
   enum :status, { draft: "draft", published: "published" }, default: :draft, validate: true
 
   validates :name, presence: true
