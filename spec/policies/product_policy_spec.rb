2ac5edf77326ca77609c760f8e36332478b27478
Uriel Juliatti Valle <ujvalle@gmail.com>
2026-09-17 09:21:53 -0300

    Use project policy spec style

--- spec/policies/product_policy_spec.rb
+++ spec/policies/product_policy_spec.rb
@@ -3,6 +3,7 @@ require "rails_helper"
 RSpec.describe ProductPolicy do
   let(:band) { create(:band) }
   let(:product) { create(:product, band: band) }
+  let(:actions) { %i[show? create? update? destroy? publish? unpublish?] }
 
   describe "Band Admin authorization" do
     it "allows an administrator of the product's band" do
@@ -10,7 +11,7 @@ RSpec.describe ProductPolicy do
       create(:band_membership, :administrator, band: band, user: user)
       policy = described_class.new(user, product)
 
-      expect(policy).to permit_actions([ :show, :create, :update, :destroy, :publish, :unpublish ])
+      actions.each { |action| expect(policy.public_send(action)).to be(true) }
     end
 
     it "does not allow a plain band member" do
@@ -18,7 +19,7 @@ RSpec.describe ProductPolicy do
       create(:band_membership, band: band, user: user)
       policy = described_class.new(user, product)
 
-      expect(policy).to forbid_actions([ :show, :create, :update, :destroy, :publish, :unpublish ])
+      actions.each { |action| expect(policy.public_send(action)).to be(false) }
     end
 
     it "does not allow an administrator of another band" do
@@ -26,14 +27,14 @@ RSpec.describe ProductPolicy do
       create(:band_membership, :administrator, band: create(:band), user: user)
       policy = described_class.new(user, product)
 
-      expect(policy).to forbid_actions([ :show, :create, :update, :destroy, :publish, :unpublish ])
+      actions.each { |action| expect(policy.public_send(action)).to be(false) }
     end
 
     it "allows a platform administrator" do
-      user = create(:user, platform_admin: true)
+      user = create(:user, :platform_admin)
       policy = described_class.new(user, product)
 
-      expect(policy).to permit_actions([ :show, :create, :update, :destroy, :publish, :unpublish ])
+      actions.each { |action| expect(policy.public_send(action)).to be(true) }
     end
   end
 end
